4efa83bd80795c889c0db2ac374f64382954decb
zackattack2130 <knoxzackery@gmail.com>
2025-09-18 18:19:28 -0700

    config.h

---
+++ config.h
@@ -0,0 +1,25 @@
+#pragma once
+
+/* Matrix */
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 7
+#define DIODE_DIRECTION COL2ROW
+
+/* Left-half matrix pins */
+#define MATRIX_ROW_PINS { F6, F7, B1 }
+#define MATRIX_COL_PINS { D3, D4, C6, D7, E6, B4, B5 }
+
+/* ===== Split over I²C (NOT serial) ===== */
+#define USE_I2C
+#define EE_HANDS
+#define NO_USB_STARTUP_CHECK
+
+/* AVR Pro Micro I²C pins (SCL=D0, SDA=D1) */
+#define I2C1_SCL_PIN D0
+#define I2C1_SDA_PIN D1
+#define I2C1_CLOCK_SPEED 100000
+#define I2C_USE_INTERNAL_PULLUPS   // helps if external pullups are weak/missing
+
+/* QoL */
+#define DEBOUNCE 5
+#define TAPPING_TERM 200
