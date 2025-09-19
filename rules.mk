6837025ac24853cff6511e04f0a71e76b8cfe87b
zackattack2130 <knoxzackery@gmail.com>
2025-09-18 18:21:30 -0700

    rules.mk

---
+++ rules.mk
@@ -0,0 +1,12 @@
+# --- com keyboard: rules ---
+
+# Split keyboard enabled
+SPLIT_KEYBOARD = yes
+
+# Use I²C for the inter-half link
+I2C_DRIVER = i2c_master
+# (Do NOT set SERIAL_DRIVER here)
+
+# No OLEDs
+OLED_ENABLE = no
+WPM_ENABLE  = no
